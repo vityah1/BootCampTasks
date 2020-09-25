65ecc755b7f6fe93d94534716ca5b149889fcd9e
vityah1 <vholoshivskiy@gmail.com>
2020-09-25 14:29:49 +0300

    add setup database

--- task3_lib.py
+++ task3_lib.py
@@ -4,10 +4,24 @@ import sqlite3
 conn = sqlite3.connect('example.db')
 c = conn.cursor()
 
+def myexit():
+    print('Do Exit...')
+    conn.close()
+    exit()
+
+def setup_database():
+# Create table
+    print ('Init...')
+    try:
+        c.execute('''CREATE TABLE authors
+     (name text, note text, rating real)''')
+    except:
+        print ('Table exist!')
+    print  ('Table check ok')
 
 def DoMenu():
     def Menu():
-        print (f'''Input:\n1 - list authors\n2 - create note\n3 - print notes\n4- Get author with the highest rating\n5 - Get author with the lowest rating\n6 - Get average rating among all authors\n0 - Exit''')
+        print (f'''MENU:\n1 - List authors\n2 - Create note\n3 - Print notes\n4 - Get author with the highest rating\n5 - Get author with the lowest rating\n6 - Get average rating among all authors\n0 - Exit''')
 
     loop=True
     while loop:
@@ -19,25 +33,25 @@ def DoMenu():
             v=int(v)
         except:
             print ('wrong input!\n')
-            exit()    
+            myexit()    
 
         if v==1:
             c.execute("select * from authors")
             print(c.fetchall())
             print ('\n')
         elif v==2:
-            author=input('input author: ')
-            note=input('input note: ')
-            rate=input('input rate [0,1]: ')
+            author=input('Input author: ')
+            note=input('Input note: ')
+            rate=input('Input rate [0,1]: ')
 
             try:
                 rate=int(rate)
             except:
-                print ('wrong input!\n')
+                print ('Wrong input!\n')
                 exit()     
             if rate not in [1,0]:
-                print ('wrong input! rate must be only 1 or 0\n')
-                exit()     
+                print ('Wrong input! rate must be only 1 or 0\n')
+                myexit()     
 
             c.execute(f'''INSERT INTO authors VALUES ('{author}','{note}',{rate}) ''')
             conn.commit()
@@ -51,12 +65,14 @@ def DoMenu():
         elif v==6:
             c.execute("select avg(rating) from authors")
             print(c.fetchall())
+
         elif v==0:
             print('Exit')
             loop=False
         else:
-            print ('wrong input!\n')
+            print ('Wrong input!\n')
     return v
 
+setup_database()
 DoMenu()
-conn.close()
+myexit()
