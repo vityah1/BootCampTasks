36b85e8b2d268ecbaff39515ce08bcdbff3b11db
vityah1 <vholoshivskiy@gmail.com>
2020-09-25 14:04:15 +0300

    cycle menu

--- task3_lib.py
+++ task3_lib.py
@@ -4,44 +4,56 @@ import sqlite3
 conn = sqlite3.connect('example.db')
 c = conn.cursor()
 
-print ("Input:\n1 - list authors\n2 - create note\n3 - print notes\n4- Get author with the highest rating\n5 - Get author with the lowest rating\n6 - Get average rating among all authors\n")
-v=input()
-print (f'''you input: {v}\n''')
 
-try:
-    v=int(v)
-except:
-    print ('wrong input!\n')
-    exit()    
+def DoMenu():
+    def Menu():
+        print ("Input:\n1 - list authors\n2 - create note\n3 - print notes\n4- Get author with the highest rating\n5 - Get author with the lowest rating\n6 - Get average rating among all authors\n0 - Exit")
 
-if v==1:
-    c.execute("select * from authors")
-    print(c.fetchall())
-    print ('\n')
-elif v==2:
-    author=input('input author: ')
-    note=input('input note: ')
-    rate=input('input rate: ')
+    loop=True
+    while loop:
+        Menu()
+        v=input()
+        print (f'''you input: {v}\n''')
 
-    try:
-        rate=int(rate)
-    except:
-        print ('wrong input!\n')
-        exit()     
+        try:
+            v=int(v)
+        except:
+            print ('wrong input!\n')
+            exit()    
 
-    c.execute(f'''INSERT INTO authors VALUES ('{author}','{note}',{rate}) ''')
-    conn.commit()
-    print ('''{author}','{note}',{rate} added\n''')
-elif v==4:
-    c.execute("select name,max(rating) from authors group by name limit 1")
-    print(c.fetchall())
-elif v==5:
-    c.execute("select name,min(rating) from authors group by name limit 1")
-    print(c.fetchall())
-elif v==6:
-    c.execute("select avg(rating) from authors")
-    print(c.fetchall())
-else:
-    print ('wrong input!\n')
+        if v==1:
+            c.execute("select * from authors")
+            print(c.fetchall())
+            print ('\n')
+        elif v==2:
+            author=input('input author: ')
+            note=input('input note: ')
+            rate=input('input rate: ')
 
+            try:
+                rate=int(rate)
+            except:
+                print ('wrong input!\n')
+                exit()     
+
+            c.execute(f'''INSERT INTO authors VALUES ('{author}','{note}',{rate}) ''')
+            conn.commit()
+            print (f'''{author}','{note}',{rate} added\n''')
+        elif v==4:
+            c.execute("select name,max(rating) from authors group by name limit 1")
+            print(c.fetchall())
+        elif v==5:
+            c.execute("select name,min(rating) from authors group by name limit 1")
+            print(c.fetchall())
+        elif v==6:
+            c.execute("select avg(rating) from authors")
+            print(c.fetchall())
+        elif v==0:
+            print('Exit')
+            loop=False
+        else:
+            print ('wrong input!\n')
+    return v
+
+DoMenu()
 conn.close()
